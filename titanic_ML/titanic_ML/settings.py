972d233a834c0f2f9ed6d5551c499c2990737532
jonesyrubicon <jonesyrubicon@gmail.com>
2020-04-14 21:51:51 +0100

    Added Static Files

--- titanic_ML/titanic_ML/settings.py
+++ titanic_ML/titanic_ML/settings.py
@@ -115,5 +115,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+import os.path
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join('static'),)
